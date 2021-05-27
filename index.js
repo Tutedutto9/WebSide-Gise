a335de122cdc606300d7ba1d475116c028ebdbe1
Tutedutto9 <mateoduttosf@gmail.com>
2021-05-27 16:40:25 -0300

    .\.git\

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
+
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
 });
+
+navMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+});
+
